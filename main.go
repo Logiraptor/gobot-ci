b0f96f010d11b877be4206ff59a5eca1fa6cdfbd
Patrick Oyarzun <patrick.oyarzun@grafana.com>
2022-09-22 17:10:47 -0500

    Add logging for all events

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/api"
 	"gobot.io/x/gobot/platforms/sphero/bb8"
+	"gobot.io/x/gobot/platforms/sphero/ollie"
 )
 
 var bb8Address = "BB-E186"
@@ -15,42 +16,61 @@ func main() {
 	bleAdaptor := NewClientAdaptor(bb8Address)
 	bb8 := bb8.NewDriver(bleAdaptor)
 
-	work := func() {}
+	var color struct {
+		Red   int
+		Green int
+		Blue  int
+	}
+
+	work := func() {
+		// gobot.Every(1*time.Second, func() {
+		// })
+	}
 
 	robot := gobot.NewRobot("bbBot",
 		[]gobot.Connection{bleAdaptor},
 		[]gobot.Device{bb8},
 		work,
 	)
-
 	m := gobot.NewMaster()
 	m.AddRobot(robot)
-
 	api.NewAPI(m).Start()
 
 	robot.AddCommand("set_color", func(params map[string]interface{}) interface{} {
-		r, err := strconv.Atoi(params["r"].(string))
+		var err error
+		color.Red, err = strconv.Atoi(params["r"].(string))
 		if err != nil {
 			fmt.Println("Error parsing r", err)
 			return err
 		}
 
-		g, err := strconv.Atoi(params["g"].(string))
+		color.Green, err = strconv.Atoi(params["g"].(string))
 		if err != nil {
 			fmt.Println("Error parsing g", err)
 			return err
 		}
 
-		b, err := strconv.Atoi(params["b"].(string))
+		color.Blue, err = strconv.Atoi(params["b"].(string))
 		if err != nil {
 			fmt.Println("Error parsing b", err)
 			return err
 		}
 
-		fmt.Println("Setting color to", r, g, b)
-		bb8.SetRGB(uint8(r), uint8(g), uint8(b))
-
+		fmt.Println("Setting color to", color.Red, color.Green, color.Blue)
+		bb8.SetRGB(uint8(color.Red), uint8(color.Green), uint8(color.Blue))
 		return true
+	})
+
+	bb8.On(ollie.Collision, func(s interface{}) {
+		fmt.Printf("Collision detected: %v", s)
+	})
+
+	bb8.On(ollie.Error, func(s interface{}) {
+		fmt.Printf("Error detected: %v", s)
+	})
+
+	bb8.On(ollie.SensorData, func(s interface{}) {
+		fmt.Printf("Sensor Data: %v", s)
 	})
 
 	err := m.Start()
