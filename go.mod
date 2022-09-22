e555ed745f488564756318bd6ecba5fbde4611d2
Patrick Oyarzun <patrick.oyarzun@grafana.com>
2022-09-21 22:32:29 -0500

    convert to generic microservice

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/logiraptor/gobot-ci
+
+go 1.18
+
+require gobot.io/x/gobot v1.16.0
+
+require (
+	github.com/JuulLabs-OSS/cbgo v0.0.2 // indirect
+	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40 // indirect
+	github.com/creack/goselect v0.1.2 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/gofrs/uuid v4.3.0+incompatible // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/muka/go-bluetooth v0.0.0-20220830075246-0746e3a1ea53 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	go.bug.st/serial v1.4.0 // indirect
+	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
+	tinygo.org/x/bluetooth v0.5.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,29 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"time"
+	"strconv"
 
 	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/api"
 	"gobot.io/x/gobot/platforms/ble"
 	"gobot.io/x/gobot/platforms/sphero/bb8"
 )
 
-//go:generate stringer -type Status
+var bb8Address = "c2e70a57-f56d-40ad-cb3e-e3e3826a9560"
 
 func main() {
-	bleAdaptor := ble.NewClientAdaptor("BB-E186")
+	bleAdaptor := ble.NewClientAdaptor(bb8Address)
 	bb8 := bb8.NewDriver(bleAdaptor)
 
-	work := func() {
-		gobot.Every(5*time.Second, func() {
-			s := checkStatus()
-			r, g, b := statusToColor(s)
-			bb8.SetRGB(r, g, b)
-		})
-	}
+	work := func() {}
 
 	robot := gobot.NewRobot("bbBot",
 		[]gobot.Connection{bleAdaptor},
@@ -31,71 +24,32 @@ func main() {
 		work,
 	)
 
-	err := robot.Start()
+	m := gobot.NewMaster()
+	m.AddRobot(robot)
+
+	api.NewAPI(m).Start()
+
+	robot.AddCommand("set_color", func(params map[string]interface{}) interface{} {
+		r, err := strconv.Atoi(params["r"].(string))
+		if err != nil {
+			return err
+		}
+
+		g, err := strconv.Atoi(params["g"].(string))
+		if err != nil {
+			return err
+		}
+
+		b, err := strconv.Atoi(params["b"].(string))
+		if err != nil {
+			return err
+		}
+
+		bb8.SetRGB(uint8(r), uint8(g), uint8(b))
+
+		return true
+	})
+
+	err := m.Start()
 	fmt.Println(err)
-}
-
-func statusToColor(s Status) (r, g, b uint8) {
-	switch s {
-	case Failed:
-		return 255, 0, 0
-	case Error:
-		return 255, 255, 0
-	case Success:
-		return 0, 255, 0
-	case InProgress:
-		return 255, 0, 255
-	}
-	return 0, 255, 255
-}
-
-type Status int
-
-const (
-	Success Status = iota
-	InProgress
-	Failed
-	Error
-)
-
-type BuildStatus struct {
-	ID                  int         `json:"id"`
-	Slug                string      `json:"slug"`
-	Description         interface{} `json:"description"`
-	PublicKey           string      `json:"public_key"`
-	LastBuildID         int         `json:"last_build_id"`
-	LastBuildNumber     string      `json:"last_build_number"`
-	LastBuildStatus     *int        `json:"last_build_status"`
-	LastBuildResult     interface{} `json:"last_build_result"`
-	LastBuildDuration   interface{} `json:"last_build_duration"`
-	LastBuildLanguage   interface{} `json:"last_build_language"`
-	LastBuildStartedAt  time.Time   `json:"last_build_started_at"`
-	LastBuildFinishedAt interface{} `json:"last_build_finished_at"`
-	Active              bool        `json:"active"`
-}
-
-func checkStatus() Status {
-	resp, err := http.Get("https://api.travis-ci.org/repositories/Logiraptor/elang.json")
-	if err != nil {
-		fmt.Println(err)
-		return Error
-	}
-	defer resp.Body.Close()
-	var status BuildStatus
-	err = json.NewDecoder(resp.Body).Decode(&status)
-	if err != nil {
-		fmt.Println(err)
-		return Error
-	}
-
-	switch {
-	case status.LastBuildStatus == nil:
-		return InProgress
-	case *status.LastBuildStatus == 0:
-		return Success
-	case *status.LastBuildStatus != 0:
-		return Error
-	}
-	fmt.Printf("Unknown status: %+v", status)
-	return Error
 }
